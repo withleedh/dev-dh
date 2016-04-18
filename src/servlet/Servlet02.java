2d3390b5b99be730a26f44d948a1a6e501d9a8b2
DH <Dh's>
2016-04-18 18:58:25 +0900

    text/plain

--- src/servlet/Servlet02.java
+++ src/servlet/Servlet02.java
@@ -8,7 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-public class Servlet02 extends GenericServlet{
+public class Servlet02 extends GenericServlet {
 
 	/**
 	 * 
@@ -18,14 +18,13 @@ public class Servlet02 extends GenericServlet{
 	@Override
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("text-plain;charset=UTF-8");
-		
+
+		response.setContentType("text/plain;charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		
+
 		out.println("Çï·Î¿ì ¿ùµå");
 		out.print("ùÀûÞ");
-		
+
 	}
 
-	
 }
